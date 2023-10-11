1b6f154a180cc6905f5c5607a66d24f5c1f1e03d
unknown <rxeeb@yandex.ru>
2023-10-11 20:05:26 +0200

    first commit

--- src/Components/Slider/index.jsx
+++ src/Components/Slider/index.jsx
@@ -1,34 +1,30 @@
 import React, { useRef } from 'react';
-import data from './data.json'
-import style from './slider.module.scss'
-
-
+import data from './data.json';
+import style from './slider.module.scss';
 
 export const Slider = () => {
+  const slider = useRef(null);
 
-  const slider = useRef(null)
-
-  const handleLeftClick = () => slider.current.scrollLeft += slider.current.offsetWidth
-  const handleRightClick = () => slider.current.scrollLeft -= slider.current.offsetWidth
-
+  const handleLeftClick = () => (slider.current.scrollLeft += slider.current.offsetWidth);
+  const handleRightClick = () => (slider.current.scrollLeft -= slider.current.offsetWidth);
 
   return (
-    <div className={style.slider} >
-      <div  className={style.slider__track} ref={slider} >
-
-        {
-          data.map((e) => {
-            return (
-              <div className={style.slider__item} >
-                <img className={style.slider__item_img} src={e.image} alt="" />
-              </div>
-            )
-          })
-        }
-
+    <div className={style.slider}>
+      <div className={style.slider__track} ref={slider}>
+        {data.map((e) => {
+          return (
+            <div className={style.slider__item}>
+              <img className={style.slider__item_img} src={e.image} alt="" />
+            </div>
+          );
+        })}
       </div>
-      <button onClick={handleLeftClick} className={style.slider__btn_prev}>&lt;</button>
-      <button onClick={handleRightClick} className={style.slider__btn_next} >&gt;</button>
+      <button onClick={handleLeftClick} className={style.slider__btn_prev}>
+        &lt;
+      </button>
+      <button onClick={handleRightClick} className={style.slider__btn_next}>
+        &gt;
+      </button>
     </div>
-  )
-}
+  );
+};
